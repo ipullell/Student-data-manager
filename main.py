2c71e33302b7891fc8b2bf0e1ffdeffa489a3bb4
Saifullah <ipullell354@gmail.com>
2026-05-16 11:36:48 +0800

    added save to json feature

--- main.py
+++ main.py
@@ -1,67 +1,70 @@
 import json
 
-data_student = []
+# ------ Step 1: Load data in students.json file ------
+try: 
+    with open ("students.json", "r") as file:
+        student_data = json.load(file)
+except FileNotFoundError:
+    student_data = []
 
-# ------ Step 1 : Create a new student ------
+# ------ Step 2: Create a function to save data ------
+def save_data():
+    with open ("students.json", "w") as file:
+        json.dump(student_data, file, indent=4)
+
+# ------ Step 3: Create a new student ------
 def create():
     name = input("Name: ")
     grade = input("Class: ")
 
     student = {
-        "id" : len(data_student) + 1,
+        "id" : len(student_data) + 1,
         "name" : name,
         "class" : grade
     }
-    data_student.append(student)
+    student_data.append(student)
+    save_data()
 
-# ------ Step 2: Show all student ------
+# ------ Step 4: Show all student ------
 def read():
-    for student in data_student:
+    for student in student_data:
         print(f"ID: {student['id']}")
         print(f"Name: {student['name']}")
         print(f"Class: {student['class']}")
         print("-" * 45)
 
-# ------ Step 3: Change student data ------
+# ------ Step 5: Change student data ------
 def update():
-    for x in data_student:
-        print(f"ID: {x['id']}")
-        print(f"Name: {x['name']}")
-        print(f"Class: {x['class']}")
-        print("-" * 45)
+    read()
 
     update_id = int(input("Enter the student ID you want to change: "))
     new_name = input("Enter a new name: ")
     new_class = input("Enter a new class: ")
     found = False
 
-    for student in data_student:
+    for student in student_data:
         if student['id'] == update_id:
             student["name"] = new_name
             student["class"] = new_class
             found = True
             print("--- Data updated successfully ---")
-    
+            save_data()
     if not found:
         print("--- Data invalid ---")
 
-# ------ Step 4: Delete student data ------
+# ------ Step 6: Delete student data ------
 def delete():
-    for x in data_student:
-        print(f"ID: {x['id']}")
-        print(f"Name: {x['name']}")
-        print(f"Class: {x['class']}")
-        print("-" * 45)
+    read()
 
     remove_id = int(input("Enter the student ID you want to delete: "))
     found = False
 
-    for student in data_student:
+    for student in student_data:
         if student['id'] == remove_id:
-            data_student.remove(student)
+            student_data.remove(student)
             found = True
             print("--- Student deleted ---")
-
+            save_data()
     if not found:
         print("--- Sorry, the ID you entered is invalid ---")
 
@@ -94,6 +97,3 @@ while True:
         delete()
     elif choice == 5:
         break
-
-# ------ Save to json file ------
-    pass
